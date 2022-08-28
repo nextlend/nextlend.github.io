423f7f3fedbb10a0cc513fdcb765f9221f5ce229
vivienthomas <69955984+vivienthomas@users.noreply.github.com>
2022-08-28 17:15:30 +0800

    first commit

---
+++ _next/static/6327sGBI1_jsy3NR8LCqT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-a73c6ac10dbc4704.js","static/chunks/pages/index-3eb52ea6c48868f3.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
